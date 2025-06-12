756b8d4722a92fa022c410637d6eda83e35b1aac
Prashant Mishra <prashantmishra10232@gmail.com>
2025-06-12 15:11:37 +0530

    integrated questions and course routes

---
+++ routes/questions.routes.js
@@ -0,0 +1,11 @@
+import {Router} from "express";
+import {getAllQuestions, getQuestionsById, addQuestion, deleteQuestion} from "../controllers/questions.controller.js";
+import { verifyJwt } from "../middlewares/auth.middleware.js";
+
+
+const router = Router();
+
+router.route('/getAllQuestions').get(verifyJwt,getAllQuestions);
+router.route('getQuestion/:id').get(verifyJwt,getQuestionsById);
+router.route('addQuestion').post(verifyJwt,addQuestion);
+router.route('deleteQuestion').delete(verifyJwt,deleteQuestion);
