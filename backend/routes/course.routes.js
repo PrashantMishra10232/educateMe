90578c5f252a7601537589675ddb3c44ecb4e244
Prashant Mishra <prashantmishra10232@gmail.com>
2025-06-21 12:30:39 +0530

    setup all the routes

---
+++ backend/routes/course.routes.js
@@ -0,0 +1,10 @@
+import {Router} from 'express';
+import {getAllCourses, setupCourse, getCourseById, deleteCourse} from "../controllers/course.controller.js"
+import {upload} from "../middlewares/multer.middleware.js"
+import {verifyJwt} from "../middlewares/auth.middleware.js"
+
+const router = Router();
+router.route('/getAllCourses').get(verifyJwt,getAllCourses);
+router.route('/getCourse/:id').get(verifyJwt,getCourseById);
+router.route('/setupCourse').post(verifyJwt,upload.single('coverImage'),setupCourse);
+router.route('/deleteCourse').delete(verifyJwt,deleteCourse)
